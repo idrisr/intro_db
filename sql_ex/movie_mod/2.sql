26eff6ca6f0619619f39694dd280f8208fac5402
Idris Raja <idris.raja@gmail.com>
2011-10-29 14:07:16 -0700

    init commit

---
+++ sql_ex/movie_mod/2.sql
@@ -0,0 +1,23 @@
+--Insert 5-star ratings by James Cameron for all movies in the 
+--database. Leave the review date as NULL.
+
+--Movies not reviewed by James Cameron
+INSERT INTO rating
+select 
+    (select rev.rID 
+    from reviewer rev 
+    join rating rat on rev.rID = rat.rID
+    and rev.name='James Cameron') 
+rID, 
+mID,
+5, 
+NULL
+from movie
+where mID not in 
+    (--movies reviewed by James Cameron
+    select mov.mID
+    from Rating rat
+    join Reviewer rev on rat.rID = rev.rID
+    join movie mov on rat.mID = mov.mID
+    and rev.name = 'James Cameron')
+;
